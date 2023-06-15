25fb8f2c99d6007ecb9283122073249ae2a75f2d
iamvslkumar <39222923+iamvslkumar@users.noreply.github.com>
2023-06-15 19:59:37 +0530

    Update Test.java

--- src/com/practice/test/Test.java
+++ src/com/practice/test/Test.java
@@ -26,3 +26,39 @@ const Popup = () => {
 };
 
 export default Popup;
+
+
+file 2
+
+import React, { useState } from 'react';
+
+const Popup = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+
+  const handleButtonClick = () => {
+    openPopup(); // Call the function to open the pop-up
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>Open Pop-up</button>
+      {isOpen && (
+        <div className="popup">
+          <h2>This is a Pop-up Box</h2>
+          <p>Hello, this is some sample text inside the pop-up box!</p>
+          <button onClick={closePopup}>Close</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Popup;
