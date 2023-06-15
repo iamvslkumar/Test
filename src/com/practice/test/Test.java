24dedcd1f303146b49154283552b59e473ec7665
iamvslkumar <39222923+iamvslkumar@users.noreply.github.com>
2023-06-16 01:02:46 +0530

    Update Test.java

--- src/com/practice/test/Test.java
+++ src/com/practice/test/Test.java
@@ -1,64 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Popup = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const CreateJiraTicket = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [projectKey, setProjectKey] = useState("");
 
-  const openPopup = () => {
-    setIsOpen(true);
-  };
+  useEffect(() => {
+    // Get the Jira API credentials from the environment
+    const jiraCredentials = {
+      username: process.env.JIRA_USERNAME,
+      password: process.env.JIRA_PASSWORD,
+    };
 
-  const closePopup = () => {
-    setIsOpen(false);
-  };
+    // Create a new axios instance with the Jira API credentials
+    const axiosInstance = axios.create({
+      baseURL: "https://your-jira-server.com/rest/api/2/",
+      headers: {
+        Authorization: `Basic ${btoa(jiraCredentials.username + ":" + jiraCredentials.password)}`,
+      },
+    });
 
-  return (
-    <div>
-      <button onClick={openPopup}>Open Pop-up</button>
-      {isOpen && (
-        <div className="popup">
-          <h2>This is a Pop-up Box</h2>
-          <p>Hello, this is some sample text inside the pop-up box!</p>
-          <button onClick={closePopup}>Close</button>
-        </div>
-      )}
-    </div>
-  );
+    // Listen for changes to the input fields
+    setTitle(event => {
+      setTitle(event.target.value);
+    });
+    setDescription(event => {
+      setDescription(event.target.value);
+    });
+    setProjectKey(event => {
+      setProjectKey(event.target.value);
+    });
+
+    // Create a new Jira ticket when the user clicks the "Create Ticket" button
+    const createTicket = () => {
+      axiosInstance
+        .post(`/issue`, {
+          projectKey: projectKey,
+          summary: title,
+          description: description,
+        })
+        .then(response => {
+          console.log("Ticket created successfully!");
+        })
+        .catch(error => {
+          console.log("Error creating ticket:", error);
+        });
+    };
+
+    // Add the "Create Ticket" button to the DOM
+    const createTicketButton = (
+      <button onClick={createTicket}>Create Ticket</button>
+    );
+
+    return (
+      <div>
+        <h1>Create Jira Ticket</h1>
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={setTitle}
+        />
+        <br />
+        <textarea
+          rows="10"
+          cols="50"
+          placeholder="Description"
+          onChange={setDescription}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Project Key"
+          onChange={setProjectKey}
+        />
+        <br />
+        {createTicketButton}
+      </div>
+    );
+  }, []);
 };
 
-export default Popup;
-
-
-file 2
-
-import React, { useState } from 'react';
-
-const Popup = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openPopup = () => {
-    setIsOpen(true);
-  };
-
-  const closePopup = () => {
-    setIsOpen(false);
-  };
-
-  const handleButtonClick = () => {
-    openPopup(); // Call the function to open the pop-up
-  };
-
-  return (
-    <div>
-      <button onClick={handleButtonClick}>Open Pop-up</button>
-      {isOpen && (
-        <div className="popup">
-          <h2>This is a Pop-up Box</h2>
-          <p>Hello, this is some sample text inside the pop-up box!</p>
-          <button onClick={closePopup}>Close</button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Popup;
+export default CreateJiraTicket;
